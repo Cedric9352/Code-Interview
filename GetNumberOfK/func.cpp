1fb248653e780414a65b75ca262f6cdf88321603
Yujia Cheng <nuclearbuff@hotmail.com>
2018-11-28 21:58:48 +0800

    change static to unnamed namespace

--- GetNumberOfK/func.cpp
+++ GetNumberOfK/func.cpp
@@ -1,31 +1,33 @@
 #include "header.h"
 
-static int get_first_k(int *data, int length, int k, int start, int end)
-{
-    if(start > end) { return -1; }
-    int mid = (start+end)/2;
-    int mid_val = data[mid];
-    if(mid_val == k) {
-        if((mid > 0 && data[mid-1] != k) || mid == 0) { return mid; }
-        else { end = mid-1; }
-    } else if(mid_val < k) {
-        start = mid+1;
-    } else { end = mid-1; }
-    return get_first_k(data, length, k, start, end);
-}
+namespace {
+    int get_first_k(int *data, int length, int k, int start, int end)
+    {
+        if(start > end) { return -1; }
+        int mid = (start+end)/2;
+        int mid_val = data[mid];
+        if(mid_val == k) {
+            if((mid > 0 && data[mid-1] != k) || mid == 0) { return mid; }
+            else { end = mid-1; }
+        } else if(mid_val < k) {
+            start = mid+1;
+        } else { end = mid-1; }
+        return get_first_k(data, length, k, start, end);
+    }
 
-static int get_last_k(int *data, int length, int k, int start, int end)
-{
-    if(start > end) { return -1; }
-    int mid = (start+end)/2;
-    int mid_val = data[mid];
-    if(mid_val == k) {
-        if((mid < length-1 && data[mid+1] != k) || mid == length-1) { return mid; }
-        else { start = mid+1; }
-    } else if(mid_val < k) {
-        start = mid+1;
-    } else { end = mid-1; }
-    return get_last_k(data, length, k, start, end);
+    int get_last_k(int *data, int length, int k, int start, int end)
+    {
+        if(start > end) { return -1; }
+        int mid = (start+end)/2;
+        int mid_val = data[mid];
+        if(mid_val == k) {
+            if((mid < length-1 && data[mid+1] != k) || mid == length-1) { return mid; }
+            else { start = mid+1; }
+        } else if(mid_val < k) {
+            start = mid+1;
+        } else { end = mid-1; }
+        return get_last_k(data, length, k, start, end);
+    }
 }
 
 int get_number_of_k(int *data, int length, int k)
