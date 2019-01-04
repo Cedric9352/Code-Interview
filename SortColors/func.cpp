89c1a3c8b45cc6646697a80efe5961ca8df34472
Yujia Cheng <nuclearbuff@hotmail.com>
2019-01-04 20:38:59 +0800

    fix some bugs

--- SortColors/func.cpp
+++ SortColors/func.cpp
@@ -2,7 +2,9 @@
 
 /**
  * hint: if we meet a zero, we just swap it with auto-inc start position,
- *       and swap it with auto-dec end position when meet a two
+ *       and swap it with auto-dec end position when meet a two. We can just
+ *       swap and inc when we meet a zero, because we can just swapped back
+ *       0 or 1, never be 2
  */
 void sort_colors(std::vector<int> &colors)
 {
@@ -10,7 +12,7 @@ void sort_colors(std::vector<int> &colors)
     st len = colors.size(), left = 0, right = len-1, i = 0;
     while(i <= right) {
         if(colors[i] == 0) { std::swap(colors[i++], colors[left++]); }
-        else if(colors[i] == 2) { std::swap(colors[i++], colors[right--]); }
+        else if(colors[i] == 2) { std::swap(colors[i], colors[right--]); }
         else { i++; }
     }
 }
